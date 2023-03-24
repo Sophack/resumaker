aae6f2e24d1b80ebcc3574136284b44983a4d79e
unknown <philipkurianv@gmail.com>
2023-03-23 20:48:06 -0400

    first commit

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,60 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation login( $email: String!, $password: String! ) {
+    login( email: $email, password: $password, ) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser( $username: String!, $email: String!, $password: String! ) {
+    addUser( username: $username, email: $email, password: $password ) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+mutation saveBook($bookData: bookInput!) {
+  saveBook(bookData: $bookData) {
+    _id
+    username
+    bookCount
+    savedBooks {
+      bookId
+      authors
+      image
+      link
+      title
+      description
+    }
+  }
+}
+`;
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
