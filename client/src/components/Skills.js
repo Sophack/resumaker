d8210cdcc5198565d1e611891c771b6584f2f008
G. Godwin <g.godwin@Gs-MacBook-Pro.local>
2023-04-01 03:09:01 -0400

    skills and work inserted again

--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -1,12 +1,52 @@
 import React, { useState, useEffect } from 'react';
+import { GET_RESUME } from '../utils/queries';
+import { useQuery } from '@apollo/client'
 
 const Skills = (props) => {
+  const { loading, data } = useQuery(GET_RESUME);
+  let resumeData = data?.getResume || {};
 
-    return(
+  const [skillsState, setSkillsState] = useState({
+    industryKnowledge: "",
+    toolsAndTechnologies: "",
+    languages: "",
+    transferableSkills: "",
+  });
+
+  useEffect(() => {
+    if (data) {
+      setSkillsState(data.getResume);
+      console.log(skillsState);
+    }
+  }, [data]);
+
+  if (loading) {
+    return <h2>LOADING...</h2>;
+  }
+
+  if (resumeData.skills) {
+    return (
       <>
         <h2>Skills</h2>
-      </>  
+        {skillsState.map((single) => (
+          <div>
+            <div>
+              {console.log(single)}
+              <p>Industry Knowledge: {single.industryKnowledge}</p>
+              <p>Tools and Technologies: {single.toolsAndTechnologies}</p>
+              <p>Languages: {single.languages}</p>
+              <p>Transferable Skills: {single.transferableSkills}</p>
+            </div>
+          </div>
+        ))}
+      </>
     );
-} 
-
+  } else {
+    return (
+      <>
+        <h2>No Skills Data</h2>
+      </>
+    );
+  }
+};
 export default Skills;
--- client/src/components/Work.js
+++ client/src/components/Work.js
@@ -1,13 +1,54 @@
 import React, { useState, useEffect } from 'react';
+import { GET_RESUME } from '../utils/queries';
+import { useQuery } from '@apollo/client'
 
 const WorkData = () => {
+  const { loading, data } = useQuery(GET_RESUME);
+  let resumeData = data?.getResume || {};
 
-    return(
-        <>
-          <h2>Work</h2>
-        </>  
+  const [workState, setWorkState] = useState({
+    company: "",
+    roles: "",
+    startDate: "",
+    endDate: "",
+    duties: "",
+  });
+
+  useEffect(() => {
+    if (data) {
+      setWorkState(data.getResume);
+      console.log(workState);
+    }
+  }, [data]);
+
+  if (loading) {
+    return <h2>LOADING...</h2>;
+  }
+
+  if (resumeData.work) {
+    return (
+      <>
+        <h2>Work</h2>
+        {workState.map((single) => (
+          <div>
+            <div>
+              {console.log(single)}
+              <p>company: {single.company}</p>
+              <p>roles: {single.roles}</p>
+              <p>startDate: {single.startDate}</p>
+              <p>endDate: {single.endDate}</p>
+              <p>duties: {single.duties}</p>
+            </div>
+          </div>
+        ))}
+      </>
     );
-
-}
-
+  } else {
+    return (
+      <>
+        <h2>No Work Data</h2>
+      </>
+    );
+  }
+};
 export default WorkData;
