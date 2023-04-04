de338f83538852720d653ab3a50bdf2ac177bfc0
kirbyfridge <114848971+kirbyfridge@users.noreply.github.com>
2023-04-03 21:51:24 -0400

    deleted files unused files & unused things from queries and user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,6 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
-// import schema from Book.js
-const bookSchema = require('./Book');
 const resumeSchema = require('./Resume')
 
 const userSchema = new Schema(
